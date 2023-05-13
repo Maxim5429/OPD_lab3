6b0d0a370176dc72cdcd36e78cfc83e6352b4c98
Maxim_Petrov <max@itproger.com>
2023-05-14 00:02:01 +0600

    доработал проект

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ def form():
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
-        f=open('login.txt', 'r').readlines() # присвоили переменной date дату из talon.txt под указанным номером
+        f=open('login.txt', 'r').readlines()
         admin_login = f[0].strip()
         admin_password = f[1].strip()
         if (login==admin_login) and (password==admin_password):
             return("correct")
         else:
             return("incorrect")
-    #return render_template('index.html', ans=num_1 + num_2)
 
 if __name__ == '__main__': #запуск
     app.run(debug=True)
